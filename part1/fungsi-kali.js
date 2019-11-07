4c362c97705cc3d61948eeb378015671223d9834
jarigsepta <jarigsepta8@gmail.com>
2019-11-07 21:32:44 +0700

    tambah latihan

---
+++ part1/fungsi-kali.js
@@ -0,0 +1,7 @@
+function kali(a, b){
+    hasilKali = a * b;
+    console.log("Hasil kali a*b = " + hasilKali);
+}function kali(a, b){
+    hasilKali = a * b;
+    console.log("Hasil kali a*b = " + hasilKali);
+}
