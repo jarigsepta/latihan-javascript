520ab0f6421425f229282b18d98a6b723afebaf8
jarigsepta <jarigsepta8@gmail.com>
2019-11-08 14:34:22 +0700

    tambah latihan

---
+++ part1/fungsi-bagi.js
@@ -0,0 +1,11 @@
+function bagi(a,b){
+    hasilBagi = a / b;
+    return hasilBagi;
+}
+
+// memanggil fungsi
+var nilai1 = 50;
+var nilai2 = 5;
+var hasilPembagian = bagi(nilai1, nilai2);
+
+console.log(hasilPembagian); //-> 10
