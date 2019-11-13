be552dfcde7a1aab27ea066f47bad37f7152668b
jarigsepta <jarigsepta8@gmail.com>
2019-11-13 14:37:17 +0700

    tambah latihan

---
+++ part1/properti-dan-method.js
@@ -0,0 +1,41 @@
+var car = {
+    // properti
+    type: "Fiat", 
+    model: "500", 
+    color: "white",
+
+    // method
+    start: function(){
+        console.log("ini method start");
+    },
+    drive: function(){
+        console.log("ini method drive");
+    },
+    brake: function(){
+        console.log("ini method brake");
+    },
+    stop: function(){
+        console.log("ini method stop");
+    }
+    
+};var car = {
+    // properti
+    type: "Fiat", 
+    model: "500", 
+    color: "white",
+
+    // method
+    start: function(){
+        console.log("ini method start");
+    },
+    drive: function(){
+        console.log("ini method drive");
+    },
+    brake: function(){
+        console.log("ini method brake");
+    },
+    stop: function(){
+        console.log("ini method stop");
+    }
+    
+};
