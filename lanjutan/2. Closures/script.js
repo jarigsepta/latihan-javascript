1ec69d4eb3efe05f3ef796e055a4dd5a2a464eca
jarigsepta <jarigsepta8@gmail.com>
2020-03-15 19:45:21 +0700

    added local execution context

--- lanjutan/2. Closures/script.js
+++ lanjutan/2. Closures/script.js
@@ -9,12 +9,27 @@
 
 // execution phase
 
-var nama = 'Joni';
+var nama = "Joni";
 var umur = 25;
 
 console.log(hello());
 
-
 function hello() {
-    return (`Halo, saya ${nama}, saya ${umur} tahun`);;
+  return `Halo, saya ${nama}, saya ${umur} tahun`;
 }
+
+// function membuat Local Execution Context
+// yang di dalamnya terdapat creation dan execution phase
+// window
+// arguments
+// hoisting
+
+var nama = "Joni";
+var username = "@joni";
+
+function cetakURL(username) {
+  var instagramURL = "http://instagram.com/";
+  return instagramURL + username;
+}
+
+console.log(cetakURL(username));
