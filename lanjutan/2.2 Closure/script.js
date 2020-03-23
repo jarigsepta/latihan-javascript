f24021c2724eca872021504a6a5bec5fa876df99
jarigsepta <jarigsepta8@gmail.com>
2020-03-23 07:27:29 +0700

    added closure dg anonymous function

--- lanjutan/2.2 Closure/script.js
+++ lanjutan/2.2 Closure/script.js
@@ -1,8 +1,19 @@
 function init() {
-    let nama = 'Joni'; // local variable
-    function tampilNama() { // inner function (closure)
-        console.log(nama); // akses ke parent variable
-    }
-    tampilNama();
+  let nama = "Joni"; // local variable
+  function tampilNama() {
+    // inner function (closure)
+    console.log(nama); // akses ke parent variable
+  }
+  tampilNama();
 }
 init();
+
+// closure dg anonymous function
+function init() {
+  return function(nama) {
+    console.log(nama);
+  };
+}
+
+let panggilNama = init();
+panggilNama("Joko");
