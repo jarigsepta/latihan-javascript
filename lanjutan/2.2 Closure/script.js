289832193647d4393e90ee0c6c60d10e60ee6248
jarigsepta <jarigsepta8@gmail.com>
2020-03-23 11:13:45 +0700

    added factory function

--- lanjutan/2.2 Closure/script.js
+++ lanjutan/2.2 Closure/script.js
@@ -1,19 +1,33 @@
 function init() {
-  let nama = "Joni"; // local variable
-  function tampilNama() {
-    // inner function (closure)
-    console.log(nama); // akses ke parent variable
-  }
-  tampilNama();
+    let nama = "Joni"; // local variable
+    function tampilNama() {
+        // inner function (closure)
+        console.log(nama); // akses ke parent variable
+    }
+    tampilNama();
 }
 init();
 
 // closure dg anonymous function
 function init() {
-  return function(nama) {
-    console.log(nama);
-  };
+    return function (nama) {
+        console.log(nama);
+    };
 }
 
 let panggilNama = init();
 panggilNama("Joko");
+
+
+// factory function
+function ucapkanSalam(waktu) {
+    return function (nama) {
+        console.log(`Halo ${nama}, Selamat ${waktu}`);
+    }
+}
+
+let selamatPagi = ucapkanSalam('Pagi');
+let selamatSiang = ucapkanSalam('Siang');
+let selamatMalam = ucapkanSalam('Malam')
+
+console.dir(selamatMalam('Joni'));
