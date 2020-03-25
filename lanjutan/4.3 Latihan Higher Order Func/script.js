a4520a631f171f5ee9001f1d24f6a6985be849f4
jarigsepta <jarigsepta8@gmail.com>
2020-03-25 10:48:42 +0700

    added latihan higher order function

---
+++ lanjutan/4.3 Latihan Higher Order Func/script.js
@@ -0,0 +1,24 @@
+const videos = Array.from(document.querySelectorAll('[data-duration]'));
+
+let jsLanjut = videos.filter(video => video.textContent.includes('Javascript Lanjutan'))
+
+    .map(item => item.dataset.duration)
+    .map(waktu => {
+        const parts = waktu.split(':').map(part => parseFloat(part));
+        return (parts[0] * 60) + parts[1];
+    })
+
+    .reduce((total, detik) => total + detik);
+
+const jam = Math.floor(jsLanjut / 3600);
+jsLanjut = jsLanjut - jam * 3600;
+const menit = Math.floor(jsLanjut / 60);
+const detik = jsLanjut - menit * 60;
+
+
+const pDurasi = document.querySelector('.total-durasi');
+pDurasi.textContent = `${jam} Jam, ${menit} Menit, ${detik} Detik.`;
+const jmlVideo = videos.filter(video => video.textContent.includes('Javascript Lanjutan')).length;
+const pJmlVideo = document.querySelector('.jumlah-video');
+pJmlVideo.textContent = `${jmlVideo} Video.`;
+console.log(jmlVideo);
