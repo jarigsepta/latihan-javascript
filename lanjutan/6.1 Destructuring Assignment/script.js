b796fadea7b4b66a485a662ee6e7127b11c4e809
jarigsepta <jarigsepta8@gmail.com>
2020-03-28 18:56:47 +0700

    added destructuring assignment (object)

--- lanjutan/6.1 Destructuring Assignment/script.js
+++ lanjutan/6.1 Destructuring Assignment/script.js
@@ -5,3 +5,19 @@ const [a, b, c] = coba;
 console.log(a);
 console.log(b);
 console.log(c);
+
+// Object Literal
+const mhs = {
+    nama: 'Joko',
+    umur: 23,
+    email: 'joko123@gmail.com'
+};
+
+const {
+    nama,
+    umur,
+    email
+} = mhs;
+console.log(nama);
+console.log(umur);
+console.log(email);
