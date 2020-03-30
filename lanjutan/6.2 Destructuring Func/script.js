0c4dbc7ce1bbdc30598bff537660f6352af82902
jarigsepta <jarigsepta8@gmail.com>
2020-03-30 07:45:45 +0700

    added penjumlahan & perkalian

---
+++ lanjutan/6.2 Destructuring Func/script.js
@@ -0,0 +1,9 @@
+// Destructuring Function
+
+function penjumlahanPerkalian(a, b) {
+    return [a + b, a * b];
+}
+
+const [jumlah, kali] = penjumlahanPerkalian(2, 3);
+console.log(jumlah);
+console.log(kali);
