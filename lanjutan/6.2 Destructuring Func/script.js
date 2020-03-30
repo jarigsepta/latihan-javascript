95dabed34641ffca7bf7516e46017ac4e5574996
jarigsepta <jarigsepta8@gmail.com>
2020-03-30 07:55:01 +0700

    added destructuring function (object)

--- lanjutan/6.2 Destructuring Func/script.js
+++ lanjutan/6.2 Destructuring Func/script.js
@@ -4,6 +4,28 @@ function penjumlahanPerkalian(a, b) {
     return [a + b, a * b];
 }
 
-const [jumlah, kali] = penjumlahanPerkalian(2, 3);
-console.log(jumlah);
+const [jumlahh, kalii] = penjumlahanPerkalian(2, 3);
+console.log(jumlahh);
+console.log(kalii);
+
+
+// Destructuring Function Object
+function kalkulasi(a, b) {
+    return {
+        tambah: a + b,
+        kurang: a - b,
+        kali: a * b,
+        bagi: a / b
+    }
+}
+
+const {
+    bagi,
+    tambah,
+    kali,
+    kurang
+} = kalkulasi(10, 2);
+console.log(kurang);
+console.log(tambah);
+console.log(bagi);
 console.log(kali);
