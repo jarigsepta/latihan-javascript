de66ea8b72d6c38eebbb8a18114350c125c0452a
jarigsepta <jarigsepta8@gmail.com>
2020-03-31 08:23:03 +0700

    added for..of (string)

--- lanjutan/7. For..of & For..in/script.js
+++ lanjutan/7. For..of & For..in/script.js
@@ -1,6 +1,13 @@
 // for..of
+// array
 const mhs = ['Toni', 'Messi', 'Ronaldo'];
 
 for (const m of mhs) {
     console.log(m);
+}
+
+//string
+const nama = 'Kaka';
+for (const n of nama) {
+    console.log(n);
 }
