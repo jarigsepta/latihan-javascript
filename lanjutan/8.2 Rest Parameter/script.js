e0fdd05edc4a4ccf33e84dfa7ff11701b811c883
jarigsepta <jarigsepta8@gmail.com>
2020-04-02 19:37:08 +0700

    added mengubah object menjadi array dg arguments

--- lanjutan/8.2 Rest Parameter/script.js
+++ lanjutan/8.2 Rest Parameter/script.js
@@ -5,3 +5,11 @@ function myFunc(a, ...myArgs) {
 }
 
 console.log(myFunc(1, 2, 3, 4, 5));
+
+
+// dg Arguments
+function functionKu() {
+    return Array.from(arguments);
+}
+// mengubah object menjadi array
+console.log(functionKu(5, 4, 3, 2, 1));
