b91aceaa784cbfeaf64941fc5323b2988f77fab4
jarigsepta <jarigsepta8@gmail.com>
2020-04-04 18:38:25 +0700

    added contoh rest parameter dg filtering

--- lanjutan/8.2 Rest Parameter/script.js
+++ lanjutan/8.2 Rest Parameter/script.js
@@ -1,59 +1,58 @@
 // Rest Parameter
 
 function myFunc(a, ...myArgs) {
-    return myArgs;
+  return myArgs;
 }
 
 console.log(myFunc(1, 2, 3, 4, 5));
 
-
 // dg Arguments
 function functionKu() {
-    return Array.from(arguments);
+  return Array.from(arguments);
 }
 // mengubah object menjadi array
 console.log(functionKu(5, 4, 3, 2, 1));
 
-
 function jumlahkan(...angka) {
-    let total = 0;
-    for (const a of angka) {
-        total += a;
-    }
-    return total;
+  let total = 0;
+  for (const a of angka) {
+    total += a;
+  }
+  return total;
 }
 
 console.log(jumlahkan(1, 2, 3, 4, 5));
 
-
 // rest parameter dg reduce
 function penjumlahan(...angka) {
-    return angka.reduce((a, b) => a + b);
+  return angka.reduce((a, b) => a + b);
 }
 console.log(penjumlahan(1, 2, 3, 4, 5));
 
 // rest parameter dg array destructuring
-const kelompok1 = ['ROnaldo', 'Raul', 'Kaka', 'Suarez', 'Fernando'];
+const kelompok1 = ["ROnaldo", "Raul", "Kaka", "Suarez", "Fernando"];
 const [ketua, wakil, ...anggota] = kelompok1;
 console.log(anggota);
 
 // rest parameter dg object destructuring
 const team = {
-    pm: 'Ronaldo',
-    frondEnd: 'Messi',
-    backEnd: 'Dida',
-    ux: 'Pirlo',
-    devOps: 'Robert'
-}
-const {
-    pm,
-    ...myTeam
-} = team;
+  pm: "Ronaldo",
+  frondEnd: "Messi",
+  backEnd: "Dida",
+  ux: "Pirlo",
+  devOps: "Robert"
+};
+const { pm, ...myTeam } = team;
 console.log(myTeam);
-
 
 // rest parameter dg filtering
 function filterBy(type, ...values) {
-    return values.filter(v => typeof v === type);
+  return values.filter(v => typeof v === type);
 }
-console.log(filterBy('boolean', 1, 2, 'Kaka', false, 10, true, 'Ronaldo'));
+console.log(filterBy("boolean", 1, 2, "Kaka", false, 10, true, "Ronaldo"));
+
+// rest parameter dg filtering
+function filterCoba(type, ...values) {
+  return values.filter(v => typeof v === type);
+}
+console.log(filterCoba("string", "Joko", "Joni", 12, false, 19, true, "Aldo"));
