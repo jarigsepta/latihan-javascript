4e57e5f92e8299d559ae066018b18386cd22e490
jarigsepta <jarigsepta8@gmail.com>
2020-04-06 21:24:03 +0700

    added synchronous callback

---
+++ lanjutan/10. Callback/script.js
@@ -0,0 +1,11 @@
+// Synchronous Callback
+function halo(nama) {
+    alert(`Halo, ${nama}`);
+}
+
+function tampilkanPesan(callback) {
+    const nama = prompt('Masukkan Nama : ');
+    callback(nama);
+}
+
+tampilkanPesan(halo);
