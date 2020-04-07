d96ed1d3dd23dc8488c349ce196834cff9f92456
jarigsepta <jarigsepta8@gmail.com>
2020-04-07 17:12:18 +0700

    added asynchronous callback

--- lanjutan/10. Callback/script.js
+++ lanjutan/10. Callback/script.js
@@ -9,3 +9,26 @@ function tampilkanPesan(callback) {
 }
 
 tampilkanPesan(halo);
+
+
+// asynchronous callback
+function getDataMahasiswa(url, success, error) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                success(xhr.response);
+            } else if (xhr.status === 404) {
+                error();
+            }
+        }
+    }
+    xhr.open('get', url);
+    xhr.send();
+}
+getDataMahasiswa('mahasiswa.json', results => {
+    console.log(JSON.parse(results));
+}, () => {
+
+})
